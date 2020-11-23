214096bdb115f235c3b234c69a66b084b7a71e5b
Kasper Siig <kasper@siig.tech>
2020-11-23 20:07:14 +0100

    Runs Ansible in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 sudo apt update && \
 sudo apt upgrade -y && \
-sudo apt install -y python3 python3-pip ansible
+sudo apt install -y python3 python3-pip ansible && \
+git clone https://github.com/KSiig/config ~/config && \
+cd ~/config && \
+ansible-playbook -K playbook.yml

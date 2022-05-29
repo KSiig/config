f847a9e638ddd5b993f2b390615b0d788e80e4f6
Kasper Siig <kasper@siig.tech>
2022-05-29 07:21:14 +0000

    Install ansible using pip3

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ if [[ ! -d ~/config ]]
 then
   sudo apt update && \
   sudo apt upgrade -y && \
-  sudo apt install -y python3 python3-pip ansible && \
+  sudo apt install -y python3 python3-pip && \
+  pip3 install ansible && \
   git clone https://github.com/KSiig/config ~/config && \
   cd ~/config && \
   ansible-playbook -K playbook.yml
